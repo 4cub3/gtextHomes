0296ac835b17e5c1660b277eb65528e33d2e2171
akintunde adedoyin <akintunde_afolabi@yahoo.com>
2023-05-09 20:32:12 +0100

    ...

--- src/components/OurMainLocation/index.tsx
+++ src/components/OurMainLocation/index.tsx
@@ -14,16 +14,16 @@ const OurMainLocation: React.FC = () => {
         </div>
         <div className="flex items-center justify-center">
           <div className="grid grid-cols-1 gap-20  sm:grid-cols-3 md:grid-cols-4">
-            <figure className="bg-Nigeria bg-cover bg-center w-[40rem] h-[30rem] sm:w-[30rem] sm:h-[20rem] md:max-w-[25rem]  flex items-end justify-center relative after:absolute after:top-0 after:left-0 after:w-full after:h-full after:bg-gradient-to-b after:from-transparent after:to-slate-900">
+            <figure className="bg-Nigeria bg-cover bg-center w-[37rem] h-[30rem] sm:w-[30rem] sm:h-[20rem] md:max-w-[25rem]  flex items-end justify-center relative after:absolute after:top-0 after:left-0 after:w-full after:h-full after:bg-gradient-to-b after:from-transparent after:to-slate-900">
               <figcaption className="text-white px-6 font-bold text-[2.5rem] z-10 text-center">NIGERIA</figcaption>
             </figure>
-            <figure className="bg-Dubai bg-cover bg-center w-[40rem] h-[30rem] sm:w-[30rem] sm:h-[20rem] md:max-w-[25rem] flex items-end justify-center relative after:absolute after:top-0 after:left-0 after:w-full after:h-full after:bg-gradient-to-b after:from-transparent after:to-slate-900">
+            <figure className="bg-Dubai bg-cover bg-center w-[37rem] h-[30rem] sm:w-[30rem] sm:h-[20rem] md:max-w-[25rem] flex items-end justify-center relative after:absolute after:top-0 after:left-0 after:w-full after:h-full after:bg-gradient-to-b after:from-transparent after:to-slate-900">
               <figcaption className="text-white px-6 font-bold text-[2.5rem] z-10 text-center">DUBAI</figcaption>
             </figure>
-            <figure className="bg-UK bg-cover bg-center w-[40rem] h-[30rem] sm:w-[30rem] sm:h-[20rem] md:max-w-[25rem] flex items-end justify-center relative after:absolute after:top-0 after:left-0 after:w-full after:h-full after:bg-gradient-to-b after:from-transparent after:to-slate-900">
+            <figure className="bg-UK bg-cover bg-center w-[37rem] h-[30rem] sm:w-[30rem] sm:h-[20rem] md:max-w-[25rem] flex items-end justify-center relative after:absolute after:top-0 after:left-0 after:w-full after:h-full after:bg-gradient-to-b after:from-transparent after:to-slate-900">
               <figcaption className="text-white px-6 font-bold text-[2.5rem] z-10 text-center">UNITED KINGDOM</figcaption>
             </figure>
-            <figure className="bg-US bg-cover bg-center w-[40rem] h-[30rem] sm:w-[30rem] sm:h-[20rem] md:max-w-[25rem] flex items-end justify-center relative after:absolute after:top-0 after:left-0 after:w-full after:h-full after:bg-gradient-to-b after:from-transparent after:to-slate-900">
+            <figure className="bg-US bg-cover bg-center w-[37rem] h-[30rem] sm:w-[30rem] sm:h-[20rem] md:max-w-[25rem] flex items-end justify-center relative after:absolute after:top-0 after:left-0 after:w-full after:h-full after:bg-gradient-to-b after:from-transparent after:to-slate-900">
               <figcaption className="text-white px-6 font-bold text-[2.5rem] z-10 text-center">UNITED STATES</figcaption>
             </figure>
           </div>
